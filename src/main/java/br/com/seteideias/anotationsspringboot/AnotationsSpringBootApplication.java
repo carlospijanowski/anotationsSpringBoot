38fd5e1bcbec43790a2056f387abb8213ca8c758
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2022-12-10 16:12:41 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/anotationsspringboot/AnotationsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.anotationsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotationsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotationsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/seteideias/anotationsspringboot/AnotationsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.anotationsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotationsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
